69e43d8619099c809587cded7571c6ebbc21b07a
Piti <pitigamedeveloper@gmail.com>
2024-03-08 23:27:35 +0100

    InitialCommit

---
+++ Source/Tanks_ERE/Tanks_EREGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tanks_EREGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKS_ERE_API ATanks_EREGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
